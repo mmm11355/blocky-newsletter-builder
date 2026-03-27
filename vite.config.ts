8c03b47f3b67bf8b49d52413b5966cc880335cdc
mmm11355 <153916713+mmm11355@users.noreply.github.com>
2026-03-27 15:59:08 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/blocky-newsletter-builder/',
+  base: "/blocky-newsletter-builder/",
+  server: {
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
